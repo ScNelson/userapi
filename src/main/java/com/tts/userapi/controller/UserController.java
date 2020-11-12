f441f04340e497ea992534df2db8e35085eba857
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-11-12 09:36:42 -0500

    Change to RestController

--- src/main/java/com/tts/userapi/controller/UserController.java
+++ src/main/java/com/tts/userapi/controller/UserController.java
@@ -2,10 +2,10 @@ package com.tts.userapi.controller;
 
 import com.tts.userapi.model.User;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
     @GetMapping("/")
     public String getUsers() {
